67112be68c3d03b7c9e79a881c419e9d5c4a7b66
MaksymPoskannyi <max2000pl@gmail.com>
2025-04-25 13:51:36 +0300

    #IOS #Bootcamp #Add initial project files for Project_1_IAmRich

---
+++ Courses/Course_From Beginner to iOS App Developer/Project_1_IAmRich/Project_1_IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_1_IAmRich
+//
+//  Created by Maksym Poskannyi on 25.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Courses/Course_From Beginner to iOS App Developer/Project_1_IAmRich/Project_1_IAmRichTests/Project_1_IAmRichTests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1_IAmRichTests.swift
+//  Project_1_IAmRichTests
+//
+//  Created by Maksym Poskannyi on 25.04.2025.
+//
+
+import Testing
+@testable import Project_1_IAmRich
+
+struct Project_1_IAmRichTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
