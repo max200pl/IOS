d38e088a89cb233f7069f583a7504ed89356696c
MaksymPoskannyi <max2000pl@gmail.com>
2025-04-30 19:09:07 +0300

    #IOS #Bootcamp #Project_13_Tipsy-iOS13 #added controls

--- Courses/Course_From Beginner to iOS App Developer/Project_13_Tipsy-iOS13/Tipsy/CalculatorViewController.swift
+++ Courses/Course_From Beginner to iOS App Developer/Project_13_Tipsy-iOS13/Tipsy/CalculatorViewController.swift
@@ -1,35 +1,30 @@
 //
-//  ViewController.swift
+//  CalculatorViewController.swift
 //  Tipsy
 //
-//  Created by Angela Yu on 09/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Maksym Poskannyi on 30.04.2025.
+//  Copyright © 2025 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculatorViewController: UIViewController {
 
-    @IBOutlet weak var billTextFIeld: UITextField!
+    @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
-
+    
     @IBAction func tipChanged(_ sender: UIButton) {
-        
     }
+    
+
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
+    
+    
     @IBAction func calculatePressed(_ sender: UIButton) {
     }
 }
-
---
+++ Courses/Course_From Beginner to iOS App Developer/Project_13_Tipsy-iOS13/Tipsy/ResultsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Maksym Poskannyi on 30.04.2025.
+//  Copyright © 2025 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var settingsLabel: UILabel!
+    @IBOutlet weak var totalLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+}
